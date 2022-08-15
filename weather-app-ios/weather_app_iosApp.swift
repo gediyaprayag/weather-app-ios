83c90f4b6e0fc2a7faa220a1dd451eccbc004944
Prayag Gediya <prayaggediya60@gmail.com>
2022-08-15 11:30:59 +0530

    Initial Commit

---
+++ weather-app-ios/weather_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_iosApp.swift
+//  weather-app-ios
+//
+//  Created by Prayag Gediya on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
